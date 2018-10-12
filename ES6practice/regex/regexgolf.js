e1128a13d1116b1d3e1b41cf4cb134991e82e287
Sylph <20277075+yamitoe@users.noreply.github.com>
2018-10-12 01:59:08 -0700

    Visual Studio Code adds extra data to .exex for regex

--- ES6practice/regex/regexgolf.js
+++ ES6practice/regex/regexgolf.js
@@ -1,23 +1,40 @@
 // Fill in the regular expressions
 
-verify(/.../,
+//Criteria
+/*
+1.car and cat
+
+2.pop and prop
+
+3.ferret, ferry, and ferrari
+
+4.Any word ending in ious
+
+5.A whitespace character followed by a period, comma, colon, or semicolon
+
+6.A word longer than six letters
+
+7.A word without the letter e (or E)
+*/
+
+verify(/car|cat/,
     ["my car", "bad cats"],
     ["camper", "high art"]);
 
-verify(/.../,
+verify(/pop|prop/,
     ["pop culture", "mad props"],
     ["plop", "prrrop"]);
 
-verify(/.../,
+verify(/ferret|ferry|ferrari/,
     ["ferret", "ferry", "ferrari"],
     ["ferrum", "transfer A"]);
 
-verify(/.../,
+verify(/ious\b/,
     ["how delicious", "spacious room"],
     ["ruinous", "consciousness"]);
 
-verify(/.../,
-    ["bad punctuation ."],
+verify(/\s[\\.,:;]/,
+    ["bad punctuation ;"],
     ["escape the period"]);
 
 verify(/.../,
@@ -30,12 +47,13 @@ verify(/.../,
 
 
 function verify(regexp, yes, no) {
-// Ignore unfinished exercises
-if (regexp.source == "...") return;
-for (let str of yes) if (!regexp.test(str)) {
- console.log(`Failure to match '${str}'`);
-}
-for (let str of no) if (regexp.test(str)) {
- console.log(`Unexpected match for '${str}'`);
-}
+    // Ignore unfinished exercises
+    if (regexp.source == "...") {return};
+
+    for (let str of yes){
+        (!regexp.test(str)) ? console.log(`Failure to match '${str}'`): console.log("success, found match" ,regexp.exec(str));    
+    }
+    for (let str of no){ 
+        (regexp.test(str)) ? console.log(`Unexpected match for '${str}'Not suppose to find anything here`, regexp.exec(str)): null;
+    }
 }
