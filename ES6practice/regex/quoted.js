914e776f3e2e134b8a2f1c63be2f430ac0b7dd3c
Sylph <20277075+yamitoe@users.noreply.github.com>
2018-10-16 16:50:10 -0700

    prac 3/3 done,
    Ironically I found number.js easier to do than quoted one, if I wasnt using debuggex visualing this would be really hard to debug

--- ES6practice/regex/quoted.js
+++ ES6practice/regex/quoted.js
@@ -5,4 +5,9 @@ let text = "'I'm the cook,' he said, 'it's my job.'";
 console.log(text.replace(/'(.+?\W)'/g, '"$1"')); 
 //oh I forgot this is just one regex repeated and not one with a pipe
 //Reason why its not $1 and $2
+
+/*A better solution is 
+/(^|\W)'|'(\W|$)/g, '$1"$2'
+Since it accomendates for if they forgot to place down a special character (comma etc)*/
+
 // → "I'm the cook," he said, "it's my job."
