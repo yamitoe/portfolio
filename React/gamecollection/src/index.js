d8b3f8cd1f699611d108b37fd9e29f027f7dbd61
Sylph <20277075+yamitoe@users.noreply.github.com>
2018-11-13 23:03:03 -0800

    Wincondition inprogress
    Trying to tes situations where one can win

--- React/gamecollection/src/index.js
+++ React/gamecollection/src/index.js
@@ -7,7 +7,7 @@ const imgs = {};
 function importAll(r){
     r.keys().map(key=>imgs[key] = r(key));
 }
-//Folder,boolean reursion, regex
+//Folder,boolean reursion, regex //Webpacks 
 importAll(require.context('./tictacktoe/images',false,/\.(png|jpe?g|svg)$/));
 //Note since this is called when loading hoisting is not a thing
 
@@ -15,21 +15,52 @@ importAll(require.context('./tictacktoe/images',false,/\.(png|jpe?g|svg)$/));
 class OnClick extends Component{
     constructor(props){
         super(props);
-        this.state = {turn: true};
+        this.state = {
+            turn: true,
+            boardData: {}
+        };
         this.handleTurn = this.handleTurn.bind(this);
     }
 
     handleTurn(e,id){
-        console.log("dabbing for justice");
-        this.setState(state=>({
-            turn: !state.turn
-        }))
+        if(!this.state.boardData.hasOwnProperty(id)){
+            this.setState(state=>({
+                turn: !state.turn,
+                boardData: Object.assign(state.boardData, ({[id]: state.turn}))
+            }))
+
+            document.getElementById(id).innerHTML = 
+        `<img alt='${this.state.turn}' src='${this.state.turn ? imgs['./x.png'] : imgs['./o.png']}'/>`;
+
+        }
+        this.checkWinCondition();
+
+        /*this.setState(state=>({
+            turn: !state.turn,
+            boardData: !state.boardData.hasOwnProperty(id) ?
+             Object.assign(state.boardData, ({[id]: !state.turn})):state.boardData
+        }))*/
+    
         /*document.getElementById(id).style.cssText =
         `background-image: url("${test}");
         object-fit: scale-down;`;*/
-        document.getElementById(id).innerHTML = 
-        `<img alt='${this.state.turn}' src='${this.state.turn ? imgs['./x.png'] : imgs['./o.png']}'/>`;
+    
     }
+
+    checkWinCondition(){
+        //This really constrains the board to a 3x3 now
+        let winPatters = ['0-0','0-1','0-2','1-0','1-1','1-2','2-0','2-1','2-2'];
+        let arrX = [];
+        let arrO = [];
+
+        let myKeys = Object.keys(this.state.boardData);
+        let matchingKey = myKeys.forEach(key=>{
+            return (this.state.boardData[key] ? arrX.push(key) :
+            arrO.push(key));
+        })
+        console.log(arrX);
+    }
+    
 
     render(){
         return(
