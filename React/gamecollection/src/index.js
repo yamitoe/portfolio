9c84e42cd48ca050530641042ebca4bb56dcea96
Sylph <20277075+yamitoe@users.noreply.github.com>
2018-11-21 01:33:16 -0800

    Refractoring tictac from index

--- React/gamecollection/src/index.js
+++ React/gamecollection/src/index.js
@@ -1,80 +1,8 @@
-import React,{Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import Board from './boardgames/components/Board';
-const imgs = {};
-
-//.keys() - array of file paths
-function importAll(r){
-    r.keys().map(key=>imgs[key] = r(key));
-}
-//Folder,boolean reursion, regex //Webpacks 
-importAll(require.context('./boardgames/images',false,/\.(png|jpe?g|svg)$/));
-//Note since this is called when loading hoisting is not a thing
-
-//move this module after done
-class OnClick extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            turn: true,
-            boardData: {}
-        };
-        this.handleTurn = this.handleTurn.bind(this);
-    }
-
-    handleTurn(e,id){
-        if(!this.state.boardData.hasOwnProperty(id)){
-            this.setState(state=>({
-                turn: !state.turn,
-                boardData: Object.assign(state.boardData, ({[id]: state.turn}))
-            }),()=>this.checkWinCondition())
-
-            document.getElementById(id).innerHTML = 
-            `<img alt='${this.state.turn}' src='${this.state.turn ? imgs['./x.png'] : imgs['./o.png']}'/>`;
-        }
-    }
-
-    checkWinCondition(){
-        //This really constrains the board to a 3x3 now
-        let arrX = [];
-        let arrO = [];
-        
-        let winCondition = [['0-0','0-1','0-2'],['1-0','1-1','1-2'],['2-0','2-1','2-2'],
-        ['0-0','1-0','2-0'],['0-1','1-1','2-1'],['0-2','1-2','2-2'],
-        ['0-0','1-1','2-2'],['0-2','1-1','2-0']];
-
-        //Get keys
-        let myKeys = Object.keys(this.state.boardData);
-        //Seperate x's and o's
-        let matchingKey = myKeys.forEach(key=>{
-            return (this.state.boardData[key] ? arrX.push(key) :
-            arrO.push(key));
-        })
-
-      winCondition.forEach(key=>{
-        //(avb) equiv !(!a&!b)
-          if(this.includeKeyLength(arrX,key) || this.includeKeyLength(arrO,key)){
-            //if won, change to red
-            key.forEach(key=>document.getElementById(key).style.cssText =
-            `background-color: red;`)
-          }
-        })   
-    }
-    
-    includeKeyLength(arr,key){
-        return !key.some(value=>!arr.includes(value)) && arr.length !== 0;
-    }
-    
-
-    render(){
-        return(
-            <Board x={3} y={3} turn={this.handleTurn}/>
-        );
-    }
-    
-}
+import TickTackToe from './boardgames/components/Ticktactoe';
 
 ReactDOM.render(
-    <OnClick/>,
+    <TickTackToe/>,
     document.getElementById('root')
 );
