4209206b07ba7d2f897bee9dfa067d846e4d4500
Sylph <20277075+yamitoe@users.noreply.github.com>
2018-11-27 17:42:03 -0800

    Dark Theme, Minor css

--- React/gamecollection/src/index.js
+++ React/gamecollection/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TickTackToe from './boardgames/components/Ticktactoe';
 import Bingo from './boardgames/components/Bingo';
+import './main.css';
 
 ReactDOM.render(
     <div>
