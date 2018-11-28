2eb23f35f30ef01208f15f2cc956448e61f261a5
Sylph <20277075+yamitoe@users.noreply.github.com>
2018-11-28 01:31:07 -0800

    addeventlistner

--- React/gamecollection/src/boardgames/components/Bingo.js
+++ React/gamecollection/src/boardgames/components/Bingo.js
@@ -2,9 +2,39 @@ import React, {Component} from 'react';
 import Board from './shared/Board';
 
 class Bingo extends Component{
+    //use class field syntax
+    //events
+    componentDidMount(){
+        window.addEventListener("click", this.onclick);
+    }
+
+    componentWillUnmount(){
+        window.addEventListener("onclick", e=>this.onclick(e));
+    }
+
+    //state
+    state = {
+        test: 0,
+        stuff: 5
+    };
+
+    //Randomizer
+    random = () =>{
+
+    };
+
+    onclick = (e) =>{
+       console.log(e.target);
+    };
+
     render(){
         return(
-            <Board x={5} y={5}/>
+            <div>
+                   <Board x={5} y={5}/>
+            <span>{this.state.test} aa</span>
+          
+            </div>
+          
         )
     }
 }
--- React/gamecollection/src/boardgames/components/shared/Board.js
+++ React/gamecollection/src/boardgames/components/shared/Board.js
@@ -6,7 +6,6 @@ function Board(props){
     for(let row = 0; row < y; row++){
         let inner = [];
         for(let column = 0; column< x; column++){
-            let squareid = row+"-"+column;
             inner.push(<div className='square' onClick={turn} key={column}>
                 {data ? data : null}
             </div>);
