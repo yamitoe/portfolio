83631feec78172de98c570674517e93749dfa206
Sylph <20277075+yamitoe@users.noreply.github.com>
2019-04-11 14:33:08 -0700

    Progress but still thinking

--- 2019/School/AlgoaAssign/preorder.js
+++ 2019/School/AlgoaAssign/preorder.js
@@ -1,10 +1,37 @@
-function createTree(key=null,leftChild=null,rightChild=null){
-    let node = {
-        key,
-        leftChild,
-        rightChild
-    };
-    return node;
+let root = {
+    key: null,
+    leftChild: null,
+    rightChild: null
+};
+
+function createTree(arr){
+    for(let value of arr){
+        addNode(value);
+    }
 }
-let x = createTree("apple");
+
+function compareNode(key,currKey){
+    if(key < currKey.key){
+        if(currKey.leftChild === null){
+            addNode()
+        }
+        else{
+            compareNode(key,leftChild)
+        }
+    }
+    else if(key > root.key){
+
+    }
+
+
+function addNode(key=null,leftChild=null.rightChild=null){
+    if(root.key === null){
+        root.key = key
+    }
+    else{
+        compareNode(key,root.key);
+    }
+}
+let myArr = [20,5,10,8,9,30,15,35];
+let x = createTree(myArr);
 console.log(x);
