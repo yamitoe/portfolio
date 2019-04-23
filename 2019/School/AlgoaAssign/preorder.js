38170c682264a2694f17c2f67def41316639ebc5
Sylph <20277075+yamitoe@users.noreply.github.com>
2019-04-23 13:28:46 -0700

    Refactored and functional
    Ended up doing this as a side project instead of assignemnet

--- 2019/School/AlgoaAssign/preorder.js
+++ 2019/School/AlgoaAssign/preorder.js
@@ -1,48 +1,57 @@
-let root = {
-    key: null,
-    leftChild: null,
-    rightChild: null
-};
-
 function createTree(arr){
+    let root = {
+        key: null,
+        leftChild: null,
+        rightChild: null
+    };
+
     for(let value of arr){
-        compareNode(value);
+        compareNode(value,root);
     }
     return root;
 }
 //currKey is the path in string
-function compareNode(key,currKey='key',path=""){
+function compareNode(key,root){
+    console.log("Hi");
+    console.log(root);
     //initialize root
-    if(root.key == null){
-        addNode(key);
-    }
-    else if(key < root[currKey]){
-        if(root[currKey].leftChild == null){
-            console.log("test");
-            addNode(key,'leftChild',path);
+    if(root.key === null){
+        addNode(key,root);
+    }//root will change paths //new root each iteration
+    else if(key < root.key){
+        if(root.leftChild === null){//If leftChild not assigned add node
+            addNode(key,root,'leftChild');
         }
-        else{
-            
+        else{//If leftChild has an assigned value //Move down a node
+            console.log("Lwr");
+            compareNode(key,root.leftChild);
         }
     }
     else if(key > root.key){
-
+        if(root.rightChild === null){//If rightChild not assigned add node
+            addNode(key,root,'rightChild');
+        }
+        else{//If rightChild has an assigned value //Move down a node
+            compareNode(key,root.rightChild);
+        }
     }
 }
 
-function addNode(key=null,value,path){
-    //initialize root
-    if(root.key == null){
+function addNode(key,root,parameter){
+    //initiliaze root
+    if(root.key === null){
         root.key = key
     }
     else{
-        console.log("called?");
-        console.log(path[value]);
-        //not sure if bracket notation for accessor name
-        root[path+value] = key;
+        root[parameter] = {
+            key,
+            leftChild: null,
+            rightChild: null
+        };
     }
 
 }
-let myArr = [20,5];
+let myArr = [20,21,5,2,1,0];
 let x = createTree(myArr);
-console.log(x);
+console.log(JSON.stringify(x));
+//console.log(x.leftChild.leftChild);
