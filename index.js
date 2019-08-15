b9dfac8b338da1732190c16693c728d2633106ad
Sylph <none>
2019-08-15 12:10:53 -0700

    Practice

---
+++ index.js
@@ -0,0 +1,17 @@
+class Tree{
+    constructor(){
+        data = {};
+    }
+    
+    addNode(num){
+        this.sortNode(num);
+        
+    }
+
+    sortNode(num){
+        console.log("test");
+    }
+}
+
+let x = new Tree();
+x.addNode(1);
