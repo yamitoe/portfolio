96be68c3ec10dc88bf776978bb510b21df8019a1
Sylph <none>
2019-08-17 11:44:51 -0700

    Fixed syntac error

--- index.js
+++ index.js
@@ -9,11 +9,29 @@ class Tree{
     }
 
     sortNode(num){
-        if(this.data.root === null){
-            console.log('test');
-            //this.data.root = num;
-            console.log('test');
+        let currentNode = this.data;
+        let prop;
+        if(currentNode.root === null){
+            currentNode.root = num;
         }
+        else if(this.data.root < num){
+            currentNode.right = {root: num};
+        }
+
+        while(currentNode.root > num){
+            if(currentNode.left === undefined){
+                currentNode.left = {root: num};
+                console.log('checking temp Node'+currentNode.left);
+                console.log('a'+ JSON.stringify(this.data));
+            }
+            if(currentNode.left !== undefined){
+                currentNode = currentNode.left;
+            }
+            else{
+                return;
+            }
+        }
+
         console.log(this.data);
      
     }
@@ -23,4 +41,7 @@ return a*c;
 }
 
 let x = new Tree();
+x.addNode(5);
 x.addNode(1);
+x.addNode(7);
+x.addNode(4);
