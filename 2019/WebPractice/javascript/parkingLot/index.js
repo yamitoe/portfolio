66bd925f70ce98dd3037cfa3836c17a6ef8239f6
Yam <none>
2019-08-21 15:50:55 -0700

    Rewrote addLevel()

--- 2019/WebPractice/javascript/parkingLot/index.js
+++ 2019/WebPractice/javascript/parkingLot/index.js
@@ -4,23 +4,17 @@ class ParkingLot{
     }
 
     addLevel(level,spots){
-        let space = {
-            level,
-            spots: (spots)=>{
-                console.log('1a');
-                console.log(spots);
-                let [motor,compact,large]=spots;
-                console.log({motor,compact,large});
-                return {motor,compact,large};
-            }        
-        };
-        space.spots(spots);
-        this.parkinglot.push(space);
+        let specSpot = (spots)=>{
+            let [motor,compact,large]=spots;
+            return {motor,compact,large};
+        }
+        let spaces =  specSpot(spots);
+        this.parkinglot.push({level,spaces});       
     }
 
 }
 
 let x = new ParkingLot();
-console.log(x.addLevel(1,[1,2,3]));
+x.addLevel(1,[1,2,3]);
 console.log(x.parkinglot);
-console.log(x.parkinglot.spots);
+
