81d32e3d8cbfd0141cd253fb1e0a5f07d33e5bb2
Yam <none>
2019-08-21 15:07:52 -0700

    Inner function working

--- 2019/WebPractice/javascript/parkingLot/index.js
+++ 2019/WebPractice/javascript/parkingLot/index.js
@@ -6,18 +6,21 @@ class ParkingLot{
     addLevel(level,spots){
         let space = {
             level,
-            spots: (spots)=>[...types]={spots}        
+            spots: (spots)=>{
+                console.log('1a');
+                console.log(spots);
+                let [motor,compact,large]=spots;
+                console.log({motor,compact,large});
+                return {motor,compact,large};
+            }        
         };
+        space.spots(spots);
         this.parkinglot.push(space);
     }
 
 }
 
 let x = new ParkingLot();
-//console.log(x.addLevel(1).spots(1,2,3));
-//console.log(x.parkinglot);
-let test = (spots)=>{
-    [motor,compact,large]=spots;
-    return {motor,compact,large};
-}
-console.log(test([1,2,3]));
+console.log(x.addLevel(1,[1,2,3]));
+console.log(x.parkinglot);
+console.log(x.parkinglot.spots);
