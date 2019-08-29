912bb7583011ef8143afb6f39ca36f8e6a319da4
Yam <none>
2019-08-29 12:56:38 -0700

    Sort if entrance is level 1 and 6
    Need to implment if entrance is in the middle

--- 2019/WebPractice/javascript/parkingLot/index.js
+++ 2019/WebPractice/javascript/parkingLot/index.js
@@ -15,8 +15,16 @@ class ParkingLot{
     findClosetParking(level,carType){
         //filter only if .some returns true 
         if(carType === 'motor'){
-            return this.parkinglot.filter(val=>Object.values(val.spaces).some(w=>w))
+            let x = this.parkinglot.filter(val=>Object.values(val.spaces).some(w=>w))
             .map(w=>w.level);
+
+            x.sort((w,a)=>{
+                console.log(w%level ,a%level);
+                return (w%level) - (a%level);
+        
+            });
+            console.log(x);
+            
         }
         else if(carType === 'car'){
             return this.parkinglot.filter(val=>Object.entries(val.spaces)
@@ -50,11 +58,12 @@ x.addLevel(2,[5,0,9]);
 x.addLevel(3,[10,0,0]);
 x.addLevel(4,[51,8,9]);
 x.addLevel(5,[0,0,0]);
+x.addLevel(6,[0,1,1]);
 
 console.log(x.parkinglot);
 console.log("The filter");
-
-console.log('motor',x.findClosetParking(1,'motor'));
+let fl= 3;
+console.log('motor','Start at floor '+fl,x.findClosetParking(3,'motor'));
 console.log('car',x.findClosetParking(1,'car'));
 console.log('bus',x.findClosetParking(1,'bus'));
 //console.log(x.findClosetParking(1,'car'));
