a0d56ce0d8aa1260adbb38fd690e4265f0b5fe82
Yam <none>
2019-09-05 15:46:29 -0700

    AddItem in prog

--- 2019/WebPractice/react/eventsprac/src/App.js
+++ 2019/WebPractice/react/eventsprac/src/App.js
@@ -5,19 +5,30 @@ import sign from './images/plus-sign.svg';
 
 export function App() {
   return (
-    <div className="container-item">
-        <img src={sign} alt="image"/>
-        <h2>Seeds</h2>
-        <div className="desc">Blah blah blah </div>
+    <div id="root">
+      <section className="container-item">
+          <img src='' alt="image"/>
+          <h2>Seeds</h2>
+          <div className="desc">Blah blah blah </div>
+      </section>
+      <AddItem/>
+
     </div>
   );
 }
 
-export function AddItem(){
-  return(
-    <div className="container-addItem">
-      <img src={sign} alt="add"/>
-    </div>
-  )
+export class AddItem extends React.Component{
+  state = {myState : [{title:'1'}]};
+  addItem = ()=>this.setState(this.state.concat([{title:'2'}]))
+  render(){
+    return(
+      <section className="container-addItem">
+        <button onClick={this.addItem}>      
+          <img src={sign} alt="add"/>
+          <span>Add Item</span>
+        </button>
+      </section>
+    )
+  }
 }
 
