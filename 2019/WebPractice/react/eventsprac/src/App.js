048f04058c7023c102f4162959d2505b34cd17b3
Yam <none>
2019-09-06 12:44:15 -0700

    AddItem displaying correctly

--- 2019/WebPractice/react/eventsprac/src/App.js
+++ 2019/WebPractice/react/eventsprac/src/App.js
@@ -6,33 +6,40 @@ import sign from './images/plus-sign.svg';
 export function App() {
   return (
     <div id="root">
-      <section className="container-item">
-          <img src='' alt="image"/>
-          <h2>Seeds</h2>
-          <div className="desc">Blah blah blah </div>
-      </section>
       <AddItem/>
 
     </div>
   );
 }
 
+export function Item(props){
+  return (
+    <section className="container-item">
+        <img src='' alt="image"/>
+        <h2>{props.title}</h2>
+        <div className="desc">Blah blah blah </div>
+    </section>
+
+  );
+}
+
 export class AddItem extends React.Component{
   state = {items : [{title:'1'}]};
   addItem = ()=>this.setState(prevState=>{
-      console.log(prevState,prevState.items
-        ,prevState.items.concat([{title:'5'}]));
         return {items:prevState.items.concat([{title:'5'}])};
     }
   )
   render(){
     return(
-      <section className="container-addItem">
-        <button onClick={this.addItem}>      
-          <img src={sign} alt="add"/>
-          <span>Add Item</span>
-        </button>
-      </section>
+      <div id="root">
+        <Item title={this.state.items.map(x=>x.title)}/>
+        <section className="container-addItem">
+          <button onClick={this.addItem}>      
+            <img src={sign} alt="add"/>
+            <span>Add Item</span>
+          </button>
+        </section>
+      </div>
     )
   }
 }
