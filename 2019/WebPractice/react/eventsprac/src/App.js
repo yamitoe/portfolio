a7ca0696760c3b8db9089c87b6f256e35833a849
Yam <none>
2019-09-09 14:52:18 -0700

    Refactoring, data change

--- 2019/WebPractice/react/eventsprac/src/App.js
+++ 2019/WebPractice/react/eventsprac/src/App.js
@@ -16,16 +16,16 @@ export function App() {
 export function Item(props){
   return (
     <section className="container-item">
-        <img src={seed1} alt="image"/>
+        <img src={props.img} alt={props.title}/>
         <h2>{props.title}</h2>
-        <div className="desc">Blah blah blah </div>
+        <div className="desc">{props.desc}</div>
     </section>
 
   );
 }
 
 export class AddItem extends React.Component{
-  state = {items : [{title:'Fioretto',img:{seed1},desc:"Sweet and great"}]};
+  state = {items : [{title:'Fioretto',img:seed1,desc:"Sweet and great"}]};
   addItem = ()=>this.setState(prevState=>{
         return {items:prevState.items.concat([{title:'5'}])};
     }
@@ -33,9 +33,8 @@ export class AddItem extends React.Component{
   render(){
     return(
       <div id="root">
-        
         {this.state.items.map(x=>{
-          return <Item title={x.title} key={shortid.generate()} />
+          return <Item {...x} img={x.img} key={shortid.generate()} />
         })}
         <section className="container-addItem">
           <button onClick={this.addItem}>      
