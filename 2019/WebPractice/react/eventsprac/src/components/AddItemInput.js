e11f714e43d4655b06ca39dd5de6df27396944ec
Yam <none>
2019-09-17 12:54:01 -0700

    Looping over tagname for inputs

--- 2019/WebPractice/react/eventsprac/src/components/AddItemInput.js
+++ 2019/WebPractice/react/eventsprac/src/components/AddItemInput.js
@@ -11,6 +11,13 @@ export class AddItemInput extends React.Component{
 
   handleSubmit =(e)=>{
     e.preventDefault();
+    e.target.childNodes
+    .forEach(node=>{
+      console.log(node.tagName);
+      if(node.tagName === 'INPUT'){
+        console.log('hi')
+      }
+    })
   }
   handleClick=(e)=>{
     let x = document.getElementById('submitForm');
@@ -27,10 +34,10 @@ export class AddItemInput extends React.Component{
       <section id="handleSubmit" style={display}>
         <form onSubmit={this.handleSubmit} id="submitForm">
           <label htmlFor="title">Title:</label>
-          <input type="text" onChange={this.handleChange} id="title" />
+          <input type="text" id="title" />
 
           <label htmlFor="desc">Description:</label>
-          <input type="text" onChange={this.handleChange} id="desc" />
+          <input type="text" id="desc" />
           <button className="btnCancel">Cancel</button>
           <button>Save</button>
         </form>
