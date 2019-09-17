c18f02c953446d669586f48624346e96ed8da3e2
Yam <none>
2019-09-17 13:38:17 -0700

    Add to object by names

--- 2019/WebPractice/react/eventsprac/src/components/AddItemInput.js
+++ 2019/WebPractice/react/eventsprac/src/components/AddItemInput.js
@@ -10,14 +10,15 @@ export class AddItemInput extends React.Component{
   }
 
   handleSubmit =(e)=>{
+    let iteminputlist = {};
     e.preventDefault();
     e.target.childNodes
     .forEach(node=>{
-      console.log(node.tagName);
       if(node.tagName === 'INPUT'){
-        console.log('hi')
+        iteminputlist[node.name] = node.value;
       }
     })
+    console.log(iteminputlist);
   }
   handleClick=(e)=>{
     let x = document.getElementById('submitForm');
@@ -34,10 +35,10 @@ export class AddItemInput extends React.Component{
       <section id="handleSubmit" style={display}>
         <form onSubmit={this.handleSubmit} id="submitForm">
           <label htmlFor="title">Title:</label>
-          <input type="text" id="title" />
+          <input type="text" name="title" />
 
           <label htmlFor="desc">Description:</label>
-          <input type="text" id="desc" />
+          <input type="text" name="desc" />
           <button className="btnCancel">Cancel</button>
           <button>Save</button>
         </form>
