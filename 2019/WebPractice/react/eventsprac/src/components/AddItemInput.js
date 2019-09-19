fc4c8d230238c669cbc1a4a9b3d3a3a48ab76d50
Yam <none>
2019-09-19 11:49:24 -0700

    Css change, refactoring, img button

--- 2019/WebPractice/react/eventsprac/src/components/AddItemInput.js
+++ 2019/WebPractice/react/eventsprac/src/components/AddItemInput.js
@@ -1,40 +1,41 @@
 import React from 'react';
-export class AddItemInput extends React.Component{
-
-  componentDidMount(){
-    document.addEventListener('mousedown',this.handleClick);
+export class AddItemInput extends React.Component {
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClick);
   }
 
-  componentWillUnmount(){
-    document.removeEventListener('mousedown',this.handleClick);
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClick);
   }
 
-  handleSubmit =(e)=>{
+  handleSubmit = e => {
     let iteminputlist = {};
     e.preventDefault();
-    e.target.childNodes
-    .forEach(node=>{
-      if(node.tagName === 'INPUT'){
+    e.target.childNodes.forEach(node => {
+      if (node.tagName === 'INPUT') {
         iteminputlist[node.name] = node.value;
       }
-    })
+    });
     console.log(iteminputlist);
     this.props.addItem(iteminputlist);
-  }
-  handleClick=(e)=>{
+  };
+  handleClick = e => {
     let x = document.getElementById('submitForm');
     let btn = document.getElementsByClassName('btnCancel')[0];
 
-    if(!x.contains(e.target) || e.target === btn ){
+    if (!x.contains(e.target) || e.target === btn) {
       this.props.changeDisplay();
     }
-
-  }
-  render(){
-    let display = this.props.displayOn ? {display:'flex'} : {display:'none'};
-    return(
+  };
+  render() {
+    let display = this.props.displayOn
+      ? { display: 'flex' }
+      : { display: 'none' };
+    return (
       <section id="handleSubmit" style={display}>
         <form onSubmit={this.handleSubmit} id="submitForm">
+          <label htmlFor="img">Add Image</label>
+          <input type="file" accept="image/*" id="img" name="img" />
           <label htmlFor="title">Title:</label>
           <input type="text" name="title" id="title" />
 
@@ -44,6 +45,6 @@ export class AddItemInput extends React.Component{
           <button>Save</button>
         </form>
       </section>
-    )
+    );
   }
 }
