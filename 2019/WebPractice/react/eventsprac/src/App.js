9100d452c236c9c553c7bd45d449ca450d650409
Yam <none>
2019-10-12 15:01:35 -0700

    Web Layout

--- 2019/WebPractice/react/eventsprac/src/App.js
+++ 2019/WebPractice/react/eventsprac/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import './stylesheets/App.scss';
-import {AddItem} from './components/AddItem.js'
+import './App.css';
 
-export function App() {
+function App() {
   return (
-    <AddItem/>
+    <main>
+      <article>
+        <p>Hello, this is Kevin Doan. This is my personnal webpage.</p>
+      </article>
+      <h2>Skills</h2>
+      <ul>
+        <li>HTML 5</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>React</li>
+        <li>PHP</li>
+        <li>Java</li>
+      </ul>
+      <h2>Front-end Tools</h2>
+      <ul>
+        <li>SASS</li>
+        <li>Babel</li>
+        <li>Webpack</li>
+        <li>Jester</li>
+      </ul>
+    </main>
   );
 }
 
-
-
-
-
+export default App;
