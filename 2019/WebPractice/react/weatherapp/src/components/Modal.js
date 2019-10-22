6ec383e0b17b320f3ae2524f1af8106129008927
Sylph <20277075+yamitoe@users.noreply.github.com>
2019-10-21 18:09:20 -0700

    Modal Css done

--- 2019/WebPractice/react/weatherapp/src/components/Modal.js
+++ 2019/WebPractice/react/weatherapp/src/components/Modal.js
@@ -5,18 +5,20 @@ import "./modal.scss";
 export class Modal extends React.Component {
   render() {
     return (
-      <section className="modal">
-        <header>
-          <span className="title">Add {this.props.title}</span>
-          <button className="cancelIcon">
-            <img src={close} alt="cancel" />
-          </button>
-        </header>
-        <div className="core">{this.props.children}</div>
-        <footer>
-          <button className="btnCancle">Cancel</button>
-          <button className="btnConfirm">Confirm</button>
-        </footer>
+      <section className="modal-container">
+        <div className="modal">
+          <header>
+            <span className="title">Add {this.props.title}</span>
+            <button className="cancelIcon">
+              <img src={close} alt="cancel" />
+            </button>
+          </header>
+          <div className="core">{this.props.children}</div>
+          <footer>
+            <button className="btnConfirm">Confirm</button>
+            <button className="btnCancel">Cancel</button>
+          </footer>
+        </div>
       </section>
     );
   }
