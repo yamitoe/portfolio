3a614948708a3b73859ae94d0d113b5c90e77767
Sylph <20277075+yamitoe@users.noreply.github.com>
2019-10-22 21:53:14 -0700

    Concept test works

--- 2019/WebPractice/react/weatherapp/src/components/CityWrapper.js
+++ 2019/WebPractice/react/weatherapp/src/components/CityWrapper.js
@@ -9,11 +9,28 @@ export class CityWrapper extends React.Component {
 
   modalDisplay = ()=>this.setState(state=>({modalDisplay:!state.modalDisplay}));
 
+  onSubmit = e=>{
+    e.preventDefault();
+
+  };
+
   render() {
+    let x = [1,2,3];
     return (
       <section className="cityWrapper">
         <CityButton modalDisplay={this.modalDisplay} />
-        { this.state.modalDisplay &&<Modal title="City" modalDisplay={this.modalDisplay} />}
+        { this.state.modalDisplay &&
+        <Modal title="City" modalDisplay={this.modalDisplay} >
+          <form onSubmit={this.onSubmit}>
+            <label htmlFor="city"></label>
+            <select name="city">
+              {x.map(data=>{
+                return <option value={data}>{data}</option>
+              })}
+            </select>
+          </form>
+        </Modal>
+        }
       </section>
     );
   }
