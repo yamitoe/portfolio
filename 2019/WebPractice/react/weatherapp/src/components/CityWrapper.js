d53703d1aa70e7c85700b94d127a2b788b2933dc
Sylph <20277075+yamitoe@users.noreply.github.com>
2019-11-08 15:07:54 -0800

    Fetch errors implemeted

--- 2019/WebPractice/react/weatherapp/src/components/CityWrapper.js
+++ 2019/WebPractice/react/weatherapp/src/components/CityWrapper.js
@@ -17,16 +17,18 @@ export class CityWrapper extends React.Component {
   onSubmit = async ()=>{
     let cityname = this.state.autoCompData;
     try{
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityname}&APPID=2f8dd18e1f11a92f2feaf7c73623413c`);
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityname}&APPID=2f8dd18e1f11a92f2feaf7c73623413c&units=metric`);
+      if(response.status !== 200){
+        throw new Error("Invalid City");
+      }
       const weatherData = await response.json();
-      //console.log(JSON.stringify(myJson));
       this.setState(state=>{
         return {cityItems:state.cityItems.concat([weatherData]) }
       }
-      );
+      )
      }
      catch(err){
-
+      alert(err)
      }
   };
   //Autocomplete data
@@ -34,14 +36,26 @@ export class CityWrapper extends React.Component {
     this.setState({autoCompData: data});
   }
 
+  itemArrCond = ()=>{
+    console.log(this.state.cityItems);
+    return this.state.cityItems.length > 0 ?
+    this.state.cityItems.map(({name, main:{temp_min,temp_max,temp},...data})=>{
+      let weather = data.weather[0].main.toLowerCase();
+      let imgArr = ['snow','rain','sunny','clouds','clear'];
+      console.log(weather);
+     return <CityItem city={name} min={temp_min} max={temp_max} temp={temp}  key={shortid.generate()} />
+    }) 
+    : false;
+  }
+
+
 
   render() {
     return (
       <section className="cityWrapper">
         <CityButton modalDisplay={this.modalDisplay} />
-        { this.state.cityItems.map(({name,temp_min,temp_max})=>{
-         return <CityItem city={name} min={temp_min} max={temp_max} key={shortid.generate()} />
-        })}
+        {this.itemArrCond()}
+      
         { this.state.modalDisplay &&
         <Modal title="City" modalDisplay={this.modalDisplay} onSubmit={this.onSubmit} >
           <AutoComplete autoCompData={this.autoCompData}/>
