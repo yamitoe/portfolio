8c67a0fa2b1650829ac45c86aa89e3eb452f1c93
Sylph <20277075+yamitoe@users.noreply.github.com>
2019-11-25 15:24:07 -0800

    Swtiched to https call

--- 2019/WebPractice/react/weatherapp/src/App.js
+++ 2019/WebPractice/react/weatherapp/src/App.js
@@ -1,32 +1,16 @@
 import React from "react";
 import "./App.css";
 import { CityWrapper } from "./components/CityWrapper";
-import * as firebase from "firebase";
-//import * as firebaseui from "firebaseui";
 
-/*const firebaseConfig = {
-  apiKey: "AIzaSyDd3RhpSr-I-DfNfZLg0warbZ1JchQiWUA",
-  authDomain: "weatherapp-5c23d.firebaseapp.com",
-  databaseURL: "https://weatherapp-5c23d.firebaseio.com",
-  projectId: "weatherapp-5c23d",
-  storageBucket: "weatherapp-5c23d.appspot.com",
-  messagingSenderId: "1095925718277",
-  appId: "1:1095925718277:web:7d6856132dd8f6e1386b6c",
-  measurementId: "G-HBPVYDV8W9"
-};*/
-
-
-export default class App extends React.Component{
-  render(){
+export default class App extends React.Component {
+  render() {
     return (
       <section className="app">
         <header></header>
         <main>
           <CityWrapper />
-          <div id="firebaseui-auth-container"></div>
         </main>
       </section>
     );
   }
 }
-
--- 2019/WebPractice/react/weatherapp/src/components/CityWrapper.js
+++ 2019/WebPractice/react/weatherapp/src/components/CityWrapper.js
@@ -3,64 +3,87 @@ import { CityItem } from "./CityItem";
 import { CityButton } from "./CityButton";
 import { Modal } from "./Modal";
 import "./styleCity.scss";
-import {AutoComplete} from "./AutoComplete";
-import shortid from 'shortid';
-import fetch from 'isomorphic-fetch';
+import { AutoComplete } from "./AutoComplete";
+import shortid from "shortid";
+import es6prom from "es6-promise";
+import fetch from "isomorphic-fetch";
 
 function importAll(r) {
   let images = {};
-  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  r.keys().map((item, index) => (images[item.replace("./", "")] = r(item)));
   return images;
 }
 
-const images = importAll(require.context('../images', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(
+  require.context("../images", false, /\.(png|jpe?g|svg)$/)
+);
 
 export class CityWrapper extends React.Component {
-  state = {modalDisplay:false,
-    autoCompData:'',
-    cityItems: []
-  };
+  state = { modalDisplay: false, autoCompData: "", cityItems: [] };
 
-  modalDisplay = ()=>this.setState(state=>({modalDisplay:!state.modalDisplay}));
+  modalDisplay = () =>
+    this.setState(state => ({ modalDisplay: !state.modalDisplay }));
   //Modal data
-  onSubmit = async ()=>{
+  onSubmit = async () => {
     let cityname = this.state.autoCompData;
-    try{
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityname}&APPID=2f8dd18e1f11a92f2feaf7c73623413c&units=metric`);
-      if(response.status !== 200){
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&APPID=2f8dd18e1f11a92f2feaf7c73623413c&units=metric`
+      );
+      if (response.status !== 200) {
         throw new Error("Invalid City");
       }
       const weatherData = await response.json();
-      this.setState(state=>{
-        return {cityItems:state.cityItems.concat([weatherData]) }
-      }
-      )
-     }
-     catch(err){
-      alert(err)
-     }
+      this.setState(state => {
+        return { cityItems: state.cityItems.concat([weatherData]) };
+      });
+    } catch (err) {
+      alert(err);
+    }
   };
   //Autocomplete data
-  autoCompData= (data)=>{
-    this.setState({autoCompData: data});
-  }
+  autoCompData = data => {
+    this.setState({ autoCompData: data });
+  };
 
   //map fetched weather data
-  itemArrCond = ()=>{
-    return this.state.cityItems.map(({name, main:{temp_min,temp_max,temp},...data})=>{
-      let weather = data.weather[0].main.toLowerCase();
-      let weatherDesc = data.weather[0].description.toUpperCase();
-      let imgArr = ['snow','rain','clouds','thunderstorm','clear','mist'];
-      let currWeather = images['clear.png'];
-      imgArr.forEach(key=>{
-        if(weather === key){
-          currWeather = images[`${key}.png`];
-        }
-      })
+  itemArrCond = () => {
+    return this.state.cityItems.map(
+      ({ name, main: { temp_min, temp_max, temp }, ...data }) => {
+        let weather = data.weather[0].main.toLowerCase();
+        let weatherDesc = data.weather[0].description.toUpperCase();
+        let imgArr = [
+          "snow",
+          "rain",
+          "clouds",
+          "thunderstorm",
+          "clear",
+          "mist"
+        ];
+        let currWeather = images["clear.png"];
+        imgArr.forEach(key => {
+          if (weather === key) {
+            currWeather = images[`${key}.png`];
+          }
+        });
 
-     return <CityItem city={name} min={ Math.round(temp_min)} max={Math.round(temp_max)} 
-     temp={Math.round(temp)} desc={weatherDesc} img={currWeather} weather={weather}  key={shortid.generate()} />
-    }) 
+        return (
+          <CityItem
+            city={name}
+            min={Math.round(temp_min)}
+            max={Math.round(temp_max)}
+            temp={Math.round(temp)}
+            desc={weatherDesc}
+            img={currWeather}
+            weather={weather}
+            key={shortid.generate()}
+          />
+        );
+      }
+    );
+  };
+  componentDidMount() {
+    es6prom.polyfill();
   }
 
   render() {
@@ -68,15 +91,19 @@ export class CityWrapper extends React.Component {
       <section id="cityWrapper">
         <CityButton modalDisplay={this.modalDisplay} />
         {this.itemArrCond()}
-        { this.state.modalDisplay &&
-        <Modal title="City" modalDisplay={this.modalDisplay} onSubmit={this.onSubmit} >
-          <AutoComplete autoCompData={this.autoCompData}/>
-        </Modal>
-        }
-         <label className="switch">
-          <input type="checkbox"/>
+        {this.state.modalDisplay && (
+          <Modal
+            title="City"
+            modalDisplay={this.modalDisplay}
+            onSubmit={this.onSubmit}
+          >
+            <AutoComplete autoCompData={this.autoCompData} />
+          </Modal>
+        )}
+        <label className="switch">
+          <input type="checkbox" />
           <span className="slider round"></span>
-         </label>
+        </label>
       </section>
     );
   }
