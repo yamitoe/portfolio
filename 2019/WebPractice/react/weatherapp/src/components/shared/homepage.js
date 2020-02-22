f2935cb1a951ef7fff1dddc70876e32383e70e8b
Sylph <20277075+yamitoe@users.noreply.github.com>
2020-02-22 14:48:26 -0800

    Seperateed css props to two different folders for mainpage

--- 2019/WebPractice/react/weatherapp/src/components/shared/homepage.js
+++ 2019/WebPractice/react/weatherapp/src/components/shared/homepage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./homepage.scss";
 
 export function Homepage() {
   let displayProject = (url, title, summary, details) => {
