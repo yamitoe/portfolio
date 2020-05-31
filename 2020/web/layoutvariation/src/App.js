c23177bb8a5f6b74f70a66bda316a9dfbb8252bf
Sylph <20277075+yamitoe@users.noreply.github.com>
2020-05-30 18:55:33 -0700

    Animated background practice

---
+++ 2020/web/layoutvariation/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.scss';
+import starimg from "./imgs/starimg.jpg"
+import stars from "./imgs/stars.mp4";
+
+function App() {
+  return (
+    <section>
+      <div className="test">
+        <video poster={starimg} autoPlay loop muted src={stars} ></video>
+        <article>
+          <div>Fullscreen Background</div>
+          <button>Click here</button>
+        </article>
+      </div>
+      <div>
+        Testing other 
+      </div>
+      <div>
+        Testing other 
+      </div>
+      <div>
+        Testing other 
+      </div>
+      <div>
+        Testing other 
+      </div>
+      <div>
+        Testing other 
+      </div>
+      <div>
+        Testing other 
+      </div>
+
+
+
+    </section>
+  );
+}
+
+export default App;
