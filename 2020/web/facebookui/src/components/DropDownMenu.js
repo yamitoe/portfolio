d43dffefa26eeb2631d299067ceac8e736c13bd3
Sylph <20277075+yamitoe@users.noreply.github.com>
2020-06-25 14:32:29 -0700

    dropdown transitions added

--- 2020/web/facebookui/src/components/DropDownMenu.js
+++ 2020/web/facebookui/src/components/DropDownMenu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ReactComponent as Cog } from "../icons/cog.svg";
 import { ReactComponent as Chevron } from "../icons/chevron.svg";
+import { ReactComponent as Arrow } from "../icons/arrow.svg";
 import { ReactComponent as Bell } from "../icons/bell.svg";
 import { CSSTransition } from "react-transition-group";
 import { useState } from "react";
@@ -8,9 +9,19 @@ import { useState } from "react";
 export function DropDownMenu(props) {
   //settings
   const [activeMenu, setActiveMenu] = useState("main");
+  const [menuHeight, setMenuHeight] = useState(null);
+
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
   function DropDownItem(props) {
     return (
-      <a href="" className="dropdownmenu-item">
+      <a
+        href="#"
+        className="dropdownmenu-item"
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+      >
         <span className="icon-button">{props.lefticon}</span>
         {props.children}
         <span className="icon-right">{props.righticon}</span>
@@ -18,10 +29,43 @@ export function DropDownMenu(props) {
     );
   }
   return (
-    <div className="dropdownmenu">
-      <CSSTransition in={activeMenu === "main"} unmountOnExit timeout={500}>
-        <DropDownItem>My profile</DropDownItem>
-        <DropDownItem lefticon={<Cog />} righticon={<Chevron />}></DropDownItem>
+    <div className="dropdownmenu" style={{ height: menuHeight }}>
+      <CSSTransition
+        in={activeMenu === "main"}
+        unmountOnExit
+        timeout={500}
+        classNames="menu-primary"
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropDownItem lefticon={<Bell />}>My profile</DropDownItem>
+          <DropDownItem
+            lefticon={<Cog />}
+            goToMenu={"settings"}
+            righticon={<Chevron />}
+          >
+            Settings
+          </DropDownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === "settings"}
+        unmountOnExit
+        timeout={500}
+        classNames="menu-secondary"
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropDownItem lefticon={<Arrow />} goToMenu={"main"}>
+            Settings
+          </DropDownItem>
+          <DropDownItem>Temp</DropDownItem>
+          <DropDownItem>Temp</DropDownItem>
+          <DropDownItem>Temp</DropDownItem>
+          <DropDownItem>Temp</DropDownItem>
+          <DropDownItem>Temp</DropDownItem>
+        </div>
       </CSSTransition>
     </div>
   );
