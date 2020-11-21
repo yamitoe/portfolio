5984bb19582cc2a811d38bc2b82d8b2579700e5a
Kevin Doan <kevin.doan@student.ufv.ca>
2020-11-21 00:35:34 -0800

    For List View, added onclick highlighting

--- GroceryApp/screens/ListScreen.tsx
+++ GroceryApp/screens/ListScreen.tsx
@@ -1,7 +1,7 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import * as React from 'react';
-import {  FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
+import {useState} from 'react';
+import {  FlatList, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
@@ -9,14 +9,17 @@ const styles = StyleSheet.create({
         paddingTop: 22
     },
     item: {
-        padding: 10,
         fontSize: 18,
-        height: 42,
     },
+    view: {
+        justifyContent: "center",
+        alignItems: "flex-start",
+        height: 42,
+        padding: 10,
+    }
   });
 
 export default function ListScreen(){
-
     return(
         <View style={styles.container}>
             <FlatList
@@ -25,8 +28,17 @@ export default function ListScreen(){
                 {key: 'List6'},
                 {key: 'List1'},
                 ]}
-                renderItem={({item}) => <View style={{flex:1,backgroundColor: 'powderblue'}}><Text style={styles.item}>{item.key}</Text></View>
-            }
+                renderItem={({item}) =>(
+                    <TouchableHighlight
+                    underlayColor="hsla(187, 100%, 94%, 0.5)"
+                    onPress={() => alert('Pressed!')}
+                    >
+                        <View style={styles.view}>
+                            <Text style={styles.item}>{item.key}</Text>
+                        </View>
+                    </TouchableHighlight>
+           
+                )}
             />
         </View>
     );
