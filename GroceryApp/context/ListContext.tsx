2b049287caaa3e1776628784c2f29aa839cf1d5b
Kevin Doan <kevin.doan@student.ufv.ca>
2020-12-01 22:58:22 -0800

    Added typing to the context

--- GroceryApp/context/ListContext.tsx
+++ GroceryApp/context/ListContext.tsx
@@ -1,4 +1,4 @@
 import React from "react";
 //Global State Data //I gave it a default 
 export const ListContext = React.createContext([{key:"", title: "", data: [""]}]);
-export const ListUpdate =  React.createContext();
+export const ListUpdate =  React.createContext((arr)=>{});
