e58b7d040f714f5cafa8ca6f086b901fed190ae9
Kevin Doan <kevin.doan@student.ufv.ca>
2020-12-06 17:44:08 -0800

    Added Async Storage, so data is now cached on reload

--- GroceryApp/App.tsx
+++ GroceryApp/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
@@ -7,13 +7,14 @@ import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
 import {ListContext, ListUpdate} from './context/ListContext'
+//Local Storage
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
-  //Some mock data
-  const [listdata, setListData] = useState([
+  let mockdata = [
     {
       key:"0as",
       title: 'For dinner', 
@@ -42,7 +43,63 @@ export default function App() {
           [{item:"Chicken Broth", key:"1qqt1",time:0},
           {item:"Pepper", key:"1qqt2",time:0},
           {item:"Noodles", key:"1qqt3",time:0},]}
-  ]);
+  ];
+  //Some mock data
+  const [listdata, setListData] = useState(mockdata)
+
+  const onLoad = useEffect(()=>{
+    //  clearAll();
+    //Loading data from storage
+    getData().then(savedData=>{
+      // console.log(savedData);
+      if(savedData == null){
+        storeData(mockdata);
+      }else{
+        setListData(savedData);
+      }
+    });
+  },[])
+  //Every time rerender happens it will save to our database
+  const onUpdate = useEffect(()=>{
+    storeData(listdata);
+  })
+
+  
+
+  const storeData = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value)
+      await AsyncStorage.setItem('@storage_Key', jsonValue)
+      // console.log("Data Saved");
+    } catch (e) {
+      // saving error
+      console.log("Data Failed to save");
+    }
+  }
+
+  
+const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('@storage_Key')
+    // console.log(jsonValue);
+    // console.log("after conversion");
+    // console.log(jsonValue != null ? JSON.parse(jsonValue) : null);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch(e) {
+    // error reading value
+    console.log("Failed to fetch data");
+  }
+}
+
+const clearAll = async () => {
+  try {
+    await AsyncStorage.clear()
+  } catch(e) {
+    // clear error
+  }
+
+  console.log('Done.')
+}
 
   if (!isLoadingComplete) {
     return null;
